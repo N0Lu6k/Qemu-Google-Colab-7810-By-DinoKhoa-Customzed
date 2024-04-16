ccdc1ba44bd3164282a5766223e4ecdd39f4aa5e
N0Lu6k <154499206+N0Lu6k@users.noreply.github.com>
2024-04-16 18:56:19 +0800

    Update Win10x86.sh

--- Win10x86.sh
+++ Win10x86.sh
@@ -6,7 +6,7 @@ qemu-img create -f raw win.img 32G
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.248-1/virtio-win.iso'
 wget -O win.iso 'https://software.download.prss.microsoft.com/dbazure/Win10_22H2_English_x64v1.iso?t=212b1f9d-6ed4-417f-acc0-1dc17d7fc497&P1=1713347798&P2=601&P3=2&P4=vq9Y68iolBWe3Qxh%2bL16ZW%2b7RhiubhW9pJEXhgcUXZQxNv7K%2fPtVioZYP6yieXWsz%2frHvq0KzBjfgfGFsQlIxzjsYuKJZwOLT64lnZgcFMEBjekD1KzOSizy%2buoOP45iojgluUE4QcU%2f%2fza0eM8T155sJ%2buUAl%2fwcaODwKzlo91wZxW3QaJHDeCLlnvYHxszH%2bV8PFHctPmLOP7WG7I74Y%2b15L59beI9GvM9OfJPnTUNa5MOfAhBLgBHocPcKC6EPMa6lTsSs9oZpoOZTadjt1RwQpv2cpXRG2Ps%2b7CDc8EtC9G%2fgKo0c4JeJ4%2bNH60bNa9Z%2f1HKEaA2w0lFWB5uqg%3d%3d'
 clear
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE './ngrok tcp 5900'
 sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
